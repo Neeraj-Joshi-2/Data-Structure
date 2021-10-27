34a4917262fed3349cd28a20fa3d752b842cf0e0
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2021-10-27 22:22:30 +0530

    Delete Singly Linked List

---
+++ Singly_Linked_List/LL_4.cpp
@@ -0,0 +1,69 @@
+// DELETING THE LINKED LIST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+	public:
+	int data;
+	Node *next;
+};
+
+Node* head ;
+
+void push(int new_data)
+{
+	Node* new_node = new Node();
+	new_node->data = new_data;
+	new_node->next = head;
+	head = new_node;
+}
+
+void printList(Node *node)
+{
+    if(node==NULL)
+    {
+        cout<<"List is empty" ;
+        return ;
+    }
+	while (node != NULL)
+	{
+		cout<<" "<<node->data;
+		node = node->next;
+	}
+}
+
+void deleteList()
+{
+	Node* current = head;
+	Node* next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	head = NULL;
+}
+
+int main()
+{
+	head = NULL ;
+	push(7); 
+	push(1); 
+	push(2); 
+	push(3); 
+
+	cout<<"Created Linked list is: ";
+	printList(head);
+
+    deleteList();
+
+    cout<<"\nLinked list is: ";
+	printList(head);
+	
+	return 0;
+}
