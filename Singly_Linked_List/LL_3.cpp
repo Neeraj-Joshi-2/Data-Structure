922890406dec9ac2e56a57e17f6dace969ca721b
Neeraj Joshi <79934749+Neeraj-Joshi-2@users.noreply.github.com>
2021-10-27 22:21:02 +0530

    Deletion in Singly Linked List

---
+++ Singly_Linked_List/LL_3.cpp
@@ -0,0 +1,100 @@
+//DELETION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+	public:
+	int data;
+	Node *next;
+};
+
+Node* head ;
+
+void push(int new_data)
+{
+	Node* new_node = new Node();
+	new_node->data = new_data;
+	new_node->next = head;
+	head = new_node;
+}
+
+void printList(Node *node)
+{
+	while (node != NULL)
+	{
+		cout<<" "<<node->data;
+		node = node->next;
+	}
+}
+
+void delete_Pos(int pos)
+{
+    Node *temp1 = head;
+    if (pos == 1)
+    {
+        head = temp1->next;
+        free(temp1);
+        return;
+    }
+
+    for (int i = 0; i < pos - 2; i++)
+    {
+        temp1 = temp1->next;
+    }
+    Node *temp2 = temp1->next;
+    temp1->next = temp2->next;
+    free(temp2);
+}
+
+void deleteNode(Node* local_head, int key)
+{
+    Node* temp = local_head;
+    Node* prev = NULL;
+
+    if (temp != NULL && temp->data == key)
+    {
+        head = temp->next; 
+        delete temp;            
+        return;
+    }
+
+    else
+    {
+    while (temp != NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return;
+
+    prev->next = temp->next;
+
+    delete temp;
+    }
+}
+
+int main()
+{
+	head = NULL;
+
+	push(7); 
+	push(1); 
+	push(6); 
+	push(3); 
+	push(8); 
+
+	cout<<"Created Linked list is: ";
+	printList(head);
+
+	delete_Pos(1) ;
+    deleteNode(head , 3) ;
+
+	cout<<"\nCreated Linked list is: ";
+	printList(head);
+	
+	return 0;
+}
